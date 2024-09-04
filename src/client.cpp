be0d16a83f7adeb2da210e368202dc9b4886d15c
hamed <amzhamed@gmail.com>
2024-09-04 14:57:43 -0700

    update client

--- src/client.cpp
+++ src/client.cpp
@@ -6,39 +6,47 @@
 #include "fdbrpc/FlowTransport.h"
 #include "fdbrpc/fdbrpc.h"
 #include "flow/TLSConfig.actor.g.h"
+#include "flow/genericactors.actor.h"
+#include "flow/actorcompiler.h" // This must be the last include
 
+// Include the server interface definition
+#include "server_interface.h" // Ensure that the CountingServerInterface is declared here
+
+// Client actor function
 ACTOR Future<Void> client(NetworkAddress serverAddress)
 {
-  CountingServerInterface csi;
+  state CountingServerInterface csi;
 
-  // Set up the server endpoints on the client side
-  csi.addCount = RequestStream<int>(Endpoint({serverAddress}, UID(1, 0)));
-  csi.subtractCount = RequestStream<int>(Endpoint({serverAddress}, UID(2, 0)));
-  csi.getCount = RequestStream<ReplyPromise<int>>(Endpoint({serverAddress}, UID(3, 0)));
+  // Initialize the server interface
+  csi.addCount = RequestStream<IntWrapper>(Endpoint({serverAddress}, UID(1, 0)));
+  csi.subtractCount = RequestStream<IntWrapper>(Endpoint({serverAddress}, UID(2, 0)));
+  csi.getCount = RequestStream<ReplyPromise<IntWrapper>>(Endpoint({serverAddress}, UID(3, 0)));
 
-  // Send commands to the server
-  csi.addCount.send(5);
-  csi.subtractCount.send(2);
+  // Example of sending requests to the server
+  wait(csi.addCount.send(IntWrapper{10}));
+  wait(csi.subtractCount.send(IntWrapper{5}));
 
-  // Request the current count
-  ReplyPromise<int> finalCount;
-  csi.getCount.send(finalCount);
+  ReplyPromise<IntWrapper> reply;
+  IntWrapper currentCount = wait(csi.getCount.getReply(reply));
 
-  int value = wait(finalCount.getFuture());
-  printf("Current count is: %d\n", value);
+  TraceEvent("ClientCurrentCount").detail("Count", currentCount.value);
 
   return Void();
 }
 
 int main(int argc, char **argv)
 {
-  Error::init();
-  FlowTransport::createInstance(false, 1);
+  // Initialize the Flow network (client mode)
+  FlowTransport::createInstance(true, 1, 1, nullptr);
 
-  NetworkAddress serverAddress("10.10.1.1", 8080);
+  // Parse the server address
+  NetworkAddress serverAddress = NetworkAddress::parse("10.10.1.1", 8080);
 
-  client(serverAddress);
+  // Run the client actor
+  Future<Void> clientFuture = client(serverAddress);
 
+  // Run the network
   g_network->run();
+
   return 0;
 }
---
+++ src/server_interface.h
@@ -0,0 +1,23 @@
+#ifndef SERVER_INTERFACE_H
+#define SERVER_INTERFACE_H
+
+#include "fdbrpc/fdbrpc.h"
+#include "flow/flow.h"
+
+// This is the server interface that the client interacts with
+struct CountingServerInterface
+{
+  // Request streams for the server operations
+  RequestStream<int> addCount;
+  RequestStream<int> subtractCount;
+  RequestStream<ReplyPromise<int>> getCount;
+
+  // Serialization
+  template <class Ar>
+  void serialize(Ar &ar)
+  {
+    serializer(ar, addCount, subtractCount, getCount);
+  }
+};
+
+#endif // SERVER_INTERFACE_H
